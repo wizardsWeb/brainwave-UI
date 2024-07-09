9163a218829bb50eaa2a53576d5f649a626366d8
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-07-10 00:34:25 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button'
 
 export default function App() {
   return (
-    <div className='text-8xl underline underline-black'>
+    <>
+    <h1 className='text-3xl font-bold underline'>
       Hello World!
+    </h1>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Button className="mt-10 text-white" href="#login">
+        Something
+      </Button>
     </div>
+      <ButtonGradient />
+    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
 )
+
+// Watch later and complete this project
+// video link = https://www.youtube.com/watch?v=B91wc5dCEBA&t=63s
